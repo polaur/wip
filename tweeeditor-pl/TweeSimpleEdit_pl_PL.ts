d08d292c30a1975944d2409c2f672ceaa067ceb5
pavbaranov <pavbaranov@gmail.com>
2020-02-27 22:09:41 +0100

    Dodany plik do tłumaczenia tweeeditor

---
+++ tweeeditor-pl/TweeSimpleEdit_pl_PL.ts
@@ -0,0 +1,368 @@
+<?xml version='1.0' encoding='utf-8'?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="33"/>
+        <source>New</source>
+        <translation>Nowy</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="34"/>
+        <source>Open</source>
+        <translation>Otwórz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="35"/>
+        <source>Save</source>
+        <translation>Zapisz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="36"/>
+        <source>Save As</source>
+        <translation>Zapisz jako</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="37"/>
+        <source>Print</source>
+        <translation>Drukuj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="38"/>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="39"/>
+        <source>Exit</source>
+        <translation>Wyjdź</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="41"/>
+        <source>Add time</source>
+        <translation>Dodaj czas</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="42"/>
+        <source>Add file name</source>
+        <translation>Dodaj nazwę pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="43"/>
+        <source>Font</source>
+        <translation>Czcionka</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="44"/>
+        <source>Find</source>
+        <translation>Znajdź</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="46"/>
+        <location filename="mainwindow_slots.cpp" line="64"/>
+        <source>About Qt</source>
+        <translation>O Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="47"/>
+        <location filename="mainwindow_oncreate.cpp" line="82"/>
+        <location filename="mainwindow_slots.cpp" line="74"/>
+        <source>About TweeEditor</source>
+        <translation>O TweeEditor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="53"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="54"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="56"/>
+        <source>Alt+T</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="57"/>
+        <source>Alt+N</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="60"/>
+        <source>Create a new text</source>
+        <translation>Utwórz nowy tekst</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="61"/>
+        <source>Save the current file</source>
+        <translation>Zapisz bieżący plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="62"/>
+        <source>Save a document as another file</source>
+        <translation>Zapisz dokument jako inny plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="63"/>
+        <source>Open a text file</source>
+        <translation>Otwórz plik tekstowy</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="64"/>
+        <source>Print current document</source>
+        <translation>Wydrukuj dokument</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="65"/>
+        <source>Close current tab</source>
+        <translation>Zapisz bieżącą zakładkę</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="67"/>
+        <source>Adds the current time to the current page</source>
+        <translation>Dodaj bieżący czas do tej strony</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="68"/>
+        <source>Adds the current file name to the current page</source>
+        <translation>Dodaj nazwę dokumentu do tej strony</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="69"/>
+        <source>Sets the global text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="70"/>
+        <source>Find the text</source>
+        <translation>Wyszukaj tekst</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="81"/>
+        <source>The information about Qt.</source>
+        <translation>Informacja o Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="106"/>
+        <source>&lt;i>Ready&lt;/i></source>
+        <translation>&lt;i>Gotowe&lt;/i></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="114"/>
+        <source>&amp;File</source>
+        <translation>&amp;Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="131"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Edytuj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_oncreate.cpp" line="145"/>
+        <source>&amp;Help</source>
+        <translation>P&amp;omoc</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="31"/>
+        <source>Too full to create!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="32"/>
+        <location filename="mainwindow_slots_edit.cpp" line="73"/>
+        <location filename="mainwindow_slots_edit.cpp" line="116"/>
+        <location filename="mainwindow_slots_edit.cpp" line="186"/>
+        <location filename="special_events.cpp" line="36"/>
+        <location filename="special_events.cpp" line="76"/>
+        <source>Error!</source>
+        <translation>Błąd!</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="32"/>
+        <source>Can't create any document now because it's full.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="50"/>
+        <source>untitled-%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="62"/>
+        <source>Select a documnet</source>
+        <translation>Wybierz dokument</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="62"/>
+        <location filename="mainwindow_slots_edit.cpp" line="177"/>
+        <location filename="special_events.cpp" line="27"/>
+        <source>All files (*.*)</source>
+        <translation>Wszystkie plik (*.*)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="72"/>
+        <source>Open failed!</source>
+        <translation>Nie powiodło się otwarcie dokumentu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="73"/>
+        <source>Can't open this file.</source>
+        <translation>Nie można otworzyć pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="116"/>
+        <location filename="mainwindow_slots_edit.cpp" line="186"/>
+        <location filename="special_events.cpp" line="36"/>
+        <location filename="special_events.cpp" line="76"/>
+        <source>Can't save this file.</source>
+        <translation>Nie można zapisać pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="129"/>
+        <location filename="mainwindow_slots_edit.cpp" line="200"/>
+        <location filename="special_events.cpp" line="45"/>
+        <location filename="special_events.cpp" line="85"/>
+        <source>Save file successfully</source>
+        <translation>Plik został zapisany</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="139"/>
+        <source>This file is &lt;b>unsaved&lt;/b>.
+Do you want save it ?</source>
+        <translation>Plik &lt;b>nie jest zapisany&lt;/b>
+Chcesz go zachować?</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="162"/>
+        <location filename="special_events.cpp" line="117"/>
+        <source>You have just closed a text document!</source>
+        <translation>Dokument został zamknięty</translation>
+    </message>
+    <message>
+        <location filename="mainwindow_slots_edit.cpp" line="177"/>
+        <location filename="special_events.cpp" line="27"/>
+        <source>Save this text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="special_events.cpp" line="95"/>
+        <source>There're &lt;b>some files unsaved&lt;/b>.
+Do you want save it ?</source>
+        <translation>Istnieją &lt;b>niezachowane pliki&lt;/b>
+Chcesz je zapisać?</translation>
+    </message>
+    <message>
+        <location filename="special_events.cpp" line="146"/>
+        <source>There're &lt;b>some files unsaved&lt;/b>.
+Do you want to save them ?</source>
+        <translation>Istnieją &lt;b>niezachowane pliki&lt;/b>
+Chcesz je zapisać?</translation>
+    </message>
+</context>
+<context>
+    <name>TweeFindDialog</name>
+    <message>
+        <location filename="FindDialog.cpp" line="18"/>
+        <source>Cancel</source>
+        <translation>Porzuć</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="19"/>
+        <location filename="FindDialog.cpp" line="71"/>
+        <source>Find</source>
+        <translation>Znajdź</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="21"/>
+        <source>&amp;Find the whole word</source>
+        <translation>&amp;Wyszukaj słowo</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="22"/>
+        <source>&amp;Default option</source>
+        <translation>&amp;Domyślne ustawienia</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="23"/>
+        <source>&amp;Find backward from your cursor position</source>
+        <translation>&amp;Szukaj wstecz od pozycji kursora</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="24"/>
+        <source>&amp;Find word incasesensitively</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="25"/>
+        <source>&amp;Find word casesensitively</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="28"/>
+        <source>Find options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="36"/>
+        <source>Expanded finding options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="47"/>
+        <source>Find the word</source>
+        <translation>Szukaj słowa</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="81"/>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+    <message>
+        <location filename="FindDialog.cpp" line="82"/>
+        <source>There's no string named &lt;b>%1&lt;/b></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TweeFirstUseDialog</name>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="28"/>
+        <source>I have read this CAREFULLY and agree with it</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="29"/>
+        <source>I DO NOT accept this license and exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="30"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="31"/>
+        <source>Cancel (Exit)</source>
+        <translation>Zakończ</translation>
+    </message>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="44"/>
+        <source>Read the license and select</source>
+        <translation>Przeczytaj licencję i zatwierdź</translation>
+    </message>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="82"/>
+        <source>Error</source>
+        <translation>Błąd</translation>
+    </message>
+    <message>
+        <location filename="tweefirstusedialog.cpp" line="83"/>
+        <source>Error preparing data!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
